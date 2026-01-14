b158f9b0f1f936ee8433003d00fd995cc8f97a54
EdBigpun <ing.edbort@gmail.com>
2026-01-14 15:47:12 -0600

    Dia 4: Agregado Header con estilos glassmorphism

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [tailwind(), react()],
 });
